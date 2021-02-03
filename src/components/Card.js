9e39cbef12da2294dba454f9df0f7a3d7e4c3fb7
unknown <calanhaga@aim.com>
2021-02-02 19:24:04 -0500

    handlers set

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,4 +8,16 @@ export default function Card() {
     const [year, setYear] = useState(2021);
     const [month, setMonth] = useState(2);
     const [day, setDay] = useState(2);
+
+    const handleChangeY = e => {
+        setYear(e.target.value);
+    };
+
+    const handleChangeM = e => {
+        setMonth(e.target.value);
+    };
+
+    const handleChangeD = e => {
+        setDay(e.target.value);
+    };
 }
