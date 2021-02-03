b406c2502d378ba1abd07497fb51d3dc1b1c07ef
unknown <calanhaga@aim.com>
2021-02-02 19:51:33 -0500

    MVP

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../App.css';
 
-export default function Card() {
+function Card() {
 
     const [nasa, setNasa] = useState([]);
     const [year, setYear] = useState(2021);
@@ -25,7 +25,44 @@ useEffect(() => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${year}-${month}-${day}`)
     .then(response =>
         setNasa(response.data)
-        .catch(error => console.log(error))
-        )
+    )
+    .catch(error => console.log(error))
 }, [year, month, day])
+
+    return (
+        <div>
+            <h1>{nasa.date}</h1>
+            <img className='nasaPic' src={nasa.hdurl} />
+            <p className='description'>Description: {nasa.explanation}</p>
+            <p>Copyright: {nasa.copyright}</p>
+            <p>What day would you like to see?</p>
+            <div className='DateSelect'>
+                Year
+                <input
+                type='text'
+                placeholder='year'
+                value={year}
+                onChange={handleChangeY}
+                />
+
+                Month
+                <input
+                type='text'
+                placeholder='month'
+                value={month}
+                onChange={handleChangeM}
+                />
+
+                Day
+                <input
+                type='text'
+                placeholder='day'
+                value={day}
+                onChange={handleChangeD}
+                />
+            </div>
+        </div>
+    )
 }
+
+export default Card;
